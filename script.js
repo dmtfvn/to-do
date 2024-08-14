ddbdb872f5c03fd7ccaa545bb63ff7fa3446e638
dmtfvn <dmtfvn@gmail.com>
2024-08-14 14:29:26 +0300

    Add functionality to remove an element

--- script.js
+++ script.js
@@ -30,17 +30,12 @@ function createNewElement(value) {
   const newItem = document.createElement('li');
 
   newItem.innerHTML = `
-    <div class="icon-start">
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
-        <path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
-      </svg>
-    </div>
+    <svg class="unmark" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
+      <path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
+    </svg>
+    <svg class="mark" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
     <p>${value}</p>
-    <div class="icon-end">
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
-        <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
-      </svg>
-    </div>
+    <img src="icons/close_24dp_5F6368_FILL0_wght400_GRAD0_opsz24.png" alt="">
   `;
 
   listContainer.appendChild(newItem);
@@ -89,5 +84,15 @@ inputField.addEventListener('keydown', function (e) {
     } else {
       inputField.value = '';
     }
+  }
+});
+
+listContainer.addEventListener('click', function (e) {
+  if (e.target.tagName === 'svg') {
+    console.log('click');
+  }
+
+  if (e.target.tagName === 'IMG') {
+    e.target.parentElement.remove();
   }
 });
